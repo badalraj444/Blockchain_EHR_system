71197809c2255cb20982d1c1be83b15938097aa3
badalraj444 <badalraj444@gmail.com>
2025-11-16 00:57:43 +0530

    changed path to all

--- mongoServer/src/server.js
+++ mongoServer/src/server.js
@@ -1,38 +1,39 @@
-import express from 'express';
-import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {connectDB} from './lib/db.js';
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./lib/db.js";
 
-import authRoutes from './routes/auth.routes.js';
 import path from "path";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5002;
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
-
-
+    credentials: true,
+  })
+);
 
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../client","dist")));
-    app.get("/*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client","dist","index.html"));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client", "dist")));
+  app.use((req, res, next) => {
+    // if you want to skip API routes, check req.path and next() accordingly
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 }
 
-app.listen(PORT, ()=>{
-    console.log('Server listening on port ' + PORT);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+  connectDB();
+});
